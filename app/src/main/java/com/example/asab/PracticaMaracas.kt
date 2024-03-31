e1895bf3fc8a63f1d83401545821ef9bca75f644
Jhon <jhon.durango18@gmail.com>
2024-03-31 13:31:22 -0500

    Practicas maraca y tambor agregados

--- app/src/main/java/com/example/asab/PracticaMaracas.kt
+++ app/src/main/java/com/example/asab/PracticaMaracas.kt
@@ -1,6 +1,12 @@
 package com.example.asab
 
+import android.content.Context
 import android.content.Intent
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
@@ -8,12 +14,16 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class PracticaMaracas : AppCompatActivity() {
+
+class PracticaMaracas : AppCompatActivity(), SensorEventListener {
 
     private lateinit var BtnVolverMain : Button
+    private lateinit var sensorManager: SensorManager
+    private lateinit var maracaSound: MediaPlayer
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_practica_maracas)
@@ -24,17 +34,79 @@ class PracticaMaracas : AppCompatActivity() {
         }
 
         BtnVolverMain = findViewById(R.id.BtnVolverMaracas)
-
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        maracaSound = MediaPlayer.create(this, R.raw.maracas_sound)
+        maracaSound.isLooping = true;
     }
+    private fun playMaracaSound() {
+        if (maracaSound != null && !maracaSound.isPlaying) {
+            maracaSound.start()
+        }
+    }
+
+    var estaSeccionMaracasActiva = false // Bandera para rastrear el estado de la sección de maracas
+    var reproductorMusica: MediaPlayer? = null // Declarar reproductorMusica para el sonido
 
     override fun onResume() {
         super.onResume()
 
-        BtnVolverMain.setOnClickListener{
+        estaSeccionMaracasActiva = true // Establecer bandera al entrar a la sección de maracas
+
+        BtnVolverMain.setOnClickListener {
             PasarActivityMain()
+            estaSeccionMaracasActiva = false // Limpiar bandera al salir de la sección de maracas
+
+            // Detener sonido si se está reproduciendo
+            reproductorMusica?.stop()
+            reproductorMusica?.release() // Liberar recursos para evitar fugas
+            reproductorMusica = null
         }
 
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_GAME)
     }
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager.unregisterListener(this)
+    }
+
+    val TIEMPO_SIN_MOVIMIENTO = 1000// Milisegundos (1 segundo)
+    var ultimoTiempoMovimiento = 0L
+
+    override fun onSensorChanged(event: SensorEvent) {
+        if (event.sensor.type == Sensor.TYPE_ACCELEROMETER && estaSeccionMaracasActiva) {
+            val x = event.values[0]
+            val y = event.values[1]
+            val z = event.values[2]
+
+            val tiempoActual = System.currentTimeMillis()
+            val intensidadMovimiento = Math.sqrt(Math.pow(x.toDouble(), 2.0) + Math.pow(y.toDouble(), 2.0) + Math.pow(z.toDouble(), 2.0))
+            val volumenFactor = 0.02 // Ajusta este valor según sea necesario
+            val volumenSonido = intensidadMovimiento * volumenFactor
+
+
+            // Detección de movimiento
+            if (Math.abs(x) > 20 || Math.abs(y) > 20 || Math.abs(z) > 20) {
+                ultimoTiempoMovimiento = tiempoActual
+                // Reproducir sonido si se detecta movimiento
+                if (reproductorMusica == null) {
+                    reproductorMusica = MediaPlayer.create(this, R.raw.maracas_sound)
+                    reproductorMusica?.start()
+                }
+                reproductorMusica?.setVolume(volumenSonido.toFloat(), volumenSonido.toFloat())
+            } else {
+                // Si no hay movimiento y ha pasado el tiempo sin movimiento, detener el sonido
+                if (tiempoActual - ultimoTiempoMovimiento > TIEMPO_SIN_MOVIMIENTO) {
+                    reproductorMusica?.stop()
+                    reproductorMusica?.release()
+                    reproductorMusica = null
+                }
+
+            }
+        }
+    }
+
+
 
     private fun PasarActivityMain() {
         // Crear un Intent para la actividad de destino
@@ -42,6 +114,12 @@ class PracticaMaracas : AppCompatActivity() {
 
         // Iniciar la actividad usando el Intent
         startActivity(intent)
+    }
+
+
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        TODO("Not yet implemented")
     }
 
 }
--- app/src/main/java/com/example/asab/PracticaTambor.kt
+++ app/src/main/java/com/example/asab/PracticaTambor.kt
@@ -1,16 +1,26 @@
 package com.example.asab
 
 import android.content.Intent
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class PracticaTambor : AppCompatActivity() {
 
     private lateinit var BtnVolver : Button
+    private lateinit var BtnTambor : Button
+    private lateinit var tamborSound: MediaPlayer
+    private val soundChannel = Channel<Unit>()
+    private val coroutineScope = CoroutineScope(Dispatchers.IO)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,7 +32,33 @@ class PracticaTambor : AppCompatActivity() {
             insets
         }
 
+
+
         BtnVolver = findViewById(R.id.BtnVolverTambor)
+        tamborSound = MediaPlayer.create(this, R.raw.tambor)
+        BtnTambor = findViewById<Button>(R.id.button3)
+        BtnTambor.setOnClickListener {
+            soundChannel.trySend(Unit)
+
+            coroutineScope.launch {
+                while (true) {
+                    // Esperar a recibir una señal en el canal
+                    soundChannel.receive()
+
+                    // Reproducir el sonido del tambor
+                    tamborSound.start()
+
+                    // Esperar un segundo
+                    delay(2000)
+
+                    // Detener el sonido del tambor
+                    tamborSound.pause()
+
+                }
+            }
+        }
+
+
     }
 
     override fun onResume() {
@@ -35,7 +71,10 @@ class PracticaTambor : AppCompatActivity() {
     private fun PasarActivityMain() {
         // Crear un Intent para la actividad de destino
         val intent = Intent(this, MainActivity::class.java)
-
+        if (tamborSound.isPlaying) {
+            tamborSound.stop()
+            tamborSound.reset()
+        }
         // Iniciar la actividad usando el Intent
         startActivity(intent)
     }
